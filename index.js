e2418cfd1d60ea05a72d8c0e3957847560ff6885
Balaji <93272649+codingisf@users.noreply.github.com>
2024-07-27 17:42:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const userRoutes = require('./Routes/userRoutes');
 require('dotenv').config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 const connectDB = async () => {
